64153cce809e1221677522f8ce2b0e2236db8321
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-09-21 22:17:34 +0530

    [Jashu]Add : Polymorphism.

---
+++ PracticeProblems/PracticeProblems/Polymorphism.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class Polymorphism : BaseClass
+    {
+        public void Display()
+        {
+            Console.WriteLine("It is a regular method with no argument.");
+            Console.WriteLine("Hello");
+        }
+        public void Display(string name)
+        {
+            Console.WriteLine("It is an overloaded method with one argument.");
+            Console.WriteLine("Hello " + name);
+        }
+        public void Display(string name, int age)
+        {
+            Console.WriteLine("It is an overloaded method with two arguments.");
+            Console.WriteLine("Hello " + name + ", your age is ; " + age);
+        }
+
+        public override void Marry()
+        {
+            Console.WriteLine("It is an overridden marry method.");
+            Console.WriteLine("No, I don't marry SubbaLakshmi. I will marry Anushka Shetty.");
+        }
+    }
+}
--- PracticeProblems/PracticeProblems/Program.cs
+++ PracticeProblems/PracticeProblems/Program.cs
@@ -13,7 +13,8 @@ namespace PracticeProblems
             bool interested = true;
             while (interested)
             {
-                Console.WriteLine("Enter your choice.\n   1   -->   Class and Object.\n   2   -->   Inheritance.\n   0   -->   QUIT");
+                Console.WriteLine("Enter your choice.\n   1   -->   Class and Object.\n   2   -->   Inheritance.\n" +
+                    "   3   -->   Polymorphism.\n   0   -->   QUIT");
                 int choice = int.Parse(Console.ReadLine());
                 switch (choice)
                 {
@@ -27,6 +28,20 @@ namespace PracticeProblems
                         childClass.ShowName();
                         childClass.DisplayAge();
                         childClass.Greet();
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter your name.");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter your age.");
+                        int age = int.Parse(Console.ReadLine());    
+                        Polymorphism polymorphism = new Polymorphism();
+                        BaseClass baseClass = new BaseClass();
+                        polymorphism.Display();
+                        polymorphism.Display(name);
+                        polymorphism.Display(name, age); 
+                        baseClass.Marry();
+                        polymorphism.Marry();
                         Console.ReadLine();
                         break;
                     case 0:
