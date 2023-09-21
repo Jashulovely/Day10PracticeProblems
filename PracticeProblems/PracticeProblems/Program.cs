6b8287157a19ed73274e45cba87370bca0761967
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-09-21 21:33:21 +0530

    [Jashu]Add : Class and Object.

---
+++ PracticeProblems/PracticeProblems/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool interested = true;
+            while (interested)
+            {
+                Console.WriteLine("Enter\n   1   -->   Class and Object.\n   0   -->   QUIT");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Test test = new Test();
+                        test.Sample();
+                        Console.ReadLine();
+                        break;
+                    case 0:
+                        interested = false;
+                        Console.WriteLine("Thanks for your interest.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ PracticeProblems/PracticeProblems/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class Test
+    {
+        public void Sample()
+        {
+            Console.WriteLine("Hello World.");
+        }
+    }
+}
