dd04d3812be1eed5ab3eba68fa3b2e3a5b556d7e
jashulovely <porsu.jaswanthkumar@gmail.com>
2023-09-21 21:44:39 +0530

    [Jashu]Add : Inheritance.

---
+++ PracticeProblems/PracticeProblems/ChildClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class ChildClass : ParentClass
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Hi Everyone.");
+        }
+    }
+}
--- PracticeProblems/PracticeProblems/Program.cs
+++ PracticeProblems/PracticeProblems/Program.cs
@@ -13,13 +13,20 @@ namespace PracticeProblems
             bool interested = true;
             while (interested)
             {
-                Console.WriteLine("Enter\n   1   -->   Class and Object.\n   0   -->   QUIT");
+                Console.WriteLine("Enter your choice.\n   1   -->   Class and Object.\n   2   -->   Inheritance.\n   0   -->   QUIT");
                 int choice = int.Parse(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
                         Test test = new Test();
                         test.Sample();
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        ChildClass childClass = new ChildClass();
+                        childClass.ShowName();
+                        childClass.DisplayAge();
+                        childClass.Greet();
                         Console.ReadLine();
                         break;
                     case 0:
